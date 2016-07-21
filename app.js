ec97aff557e659dc20ea2468b63660f0a9ecef4f
Guy Bertental <guybartal@gmail.com>
2016-07-21 17:31:48 +0300

    add game logic

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 
+var io = require('socket.io')(process.env.PORT || 3000);
+var shortid = require('shortid');
+var players = [];
+
 
 var express = require('express'),
   config = require('./config/config');
@@ -11,3 +15,55 @@ app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
 
+
+io.on('connection', (socket) => {
+    var thisPlayerId = shortid.generate();
+    var player = {
+        id: thisPlayerId,
+        x: 0,
+        y: 0
+    };
+
+    players[thisPlayerId] = player;
+    console.log('client connected, broadcasting spawn, id: ', thisPlayerId);
+
+    socket.emit('register', { id: thisPlayerId });
+    socket.broadcast.emit('spawn', { id: thisPlayerId });
+    socket.broadcast.emit('requestPosition');
+
+    for (var playerId in players) {
+        if (playerId == thisPlayerId) {
+            continue;
+        }
+        socket.emit('spawn', players[playerId]);
+        console.log('sending spawn to new player for id: ', playerId);
+
+    }
+
+    socket.on('move', (data) => {
+        data.id = thisPlayerId;
+        console.log('client moved', JSON.stringify(data));
+        player.x = data.x;
+        player.y = data.y;
+        socket.broadcast.emit('move', data);
+    });
+
+    socket.on('follow', (data) => {
+        console.log('follow request', data);
+        data.id = thisPlayerId;
+        socket.broadcast.emit('follow', data);
+    });
+
+
+    socket.on('updatePosition', (data) => {
+        console.log('update position', data);
+        data.id = thisPlayerId;
+        socket.broadcast.emit('updatePosition', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('client id ', thisPlayerId, ' disconnected');
+        delete players[thisPlayerId];
+        socket.broadcast.emit('disconnected', { id: thisPlayerId });
+    });
+})
